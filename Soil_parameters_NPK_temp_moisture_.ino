be8ec9d2c1dc6df0ae143ee318b8e0c52f21987e
Kannattu <kannattu02akhil@gmail.com>
2024-06-17 13:16:00 +0530

    Add files via upload

---
+++ Soil_parameters_NPK_temp_moisture_.ino
@@ -0,0 +1,163 @@
+
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+
+// RE and DE Pins set the RS485 module
+// to Receiver or Transmitter mode
+#define RE 32
+#define DE 33
+
+#define moist_pin A0
+
+// Modbus RTU requests for reading NPK values
+const byte nitro[] = {0x01, 0x03, 0x00, 0x1e, 0x00, 0x01, 0xe4, 0x0c};
+const byte phos[] = {0x01, 0x03, 0x00, 0x1f, 0x00, 0x01, 0xb5, 0xcc};
+const byte pota[] = {0x01, 0x03, 0x00, 0x20, 0x00, 0x01, 0x85, 0xc0};
+
+// A variable used to store NPK values
+byte values[11];
+
+// Tempearture sensor pin
+const int oneWireBus = 2;     
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// Sets up a new SoftwareSerial object
+// Digital pins 10 and 11 should be used with a Mega or Mega 2560
+SoftwareSerial mod(16, 17);
+//SoftwareSerial mod(10, 11);
+
+void setup()
+{
+  // Set the baud rate for the Serial port
+  Serial.begin(9600);
+
+  // Set the baud rate for the SerialSoftware object
+  mod.begin(9600);
+  
+  // Start the DS18B20 sensor
+  sensors.begin();
+
+  // Define pin modes for RE and DE
+  pinMode(RE, OUTPUT);
+  pinMode(DE, OUTPUT);
+
+  delay(500);
+}
+
+void loop()
+{
+  // Read values
+  byte val1, val2, val3;
+  val1 = nitrogen();
+  delay(250);
+  val2 = phosphorous();
+  delay(250);
+  val3 = potassium();
+  delay(250);
+
+  // Print values to the serial monitor
+  Serial.print("Nitrogen: ");
+  Serial.print(val1);
+  Serial.println(" mg/kg");
+  Serial.print("Phosphorous: ");
+  Serial.print(val2);
+  Serial.println(" mg/kg");
+  Serial.print("Potassium: ");
+  Serial.print(val3);
+  Serial.println(" mg/kg");
+
+  temperature();
+  moisture_sensor();
+
+  delay(2000);
+}
+
+byte nitrogen()
+{
+  digitalWrite(DE, HIGH);
+  digitalWrite(RE, HIGH);
+  delay(10);
+  if (mod.write(nitro, sizeof(nitro)) == 8)
+  {
+    digitalWrite(DE, LOW);
+    digitalWrite(RE, LOW);
+    for (byte i = 0; i < 7; i++)
+    {
+      //Serial.print(mod.read(),HEX);
+      values[i] = mod.read();
+      Serial.print(values[i], HEX);
+    }
+    Serial.println();
+  }
+  return values[4];
+}
+
+byte phosphorous()
+{
+  digitalWrite(DE, HIGH);
+  digitalWrite(RE, HIGH);
+  delay(10);
+  if (mod.write(phos, sizeof(phos)) == 8)
+  {
+    digitalWrite(DE, LOW);
+    digitalWrite(RE, LOW);
+    for (byte i = 0; i < 7; i++)
+    {
+      //Serial.print(mod.read(),HEX);
+      values[i] = mod.read();
+      Serial.print(values[i], HEX);
+    }
+    Serial.println();
+  }
+  return values[4];
+}
+
+byte potassium()
+{
+  digitalWrite(DE, HIGH);
+  digitalWrite(RE, HIGH);
+  delay(10);
+  if (mod.write(pota, sizeof(pota)) == 8)
+  {
+    digitalWrite(DE, LOW);
+    digitalWrite(RE, LOW);
+    for (byte i = 0; i < 7; i++)
+    {
+      //Serial.print(mod.read(),HEX);
+      values[i] = mod.read();
+      Serial.print(values[i], HEX);
+    }
+    Serial.println();
+  }
+  return values[4];
+}
+
+void temperature() 
+{
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  float temperatureF = sensors.getTempFByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+  Serial.print(temperatureF);
+  Serial.println("ºF");
+  delay(5000);
+}
+
+void moisture_sensor()
+{
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(moist_pin);
+  int moisture = map(sensorValue,0,1024,0,100);
+  // print out the value you read:
+  Serial.println(moisture);
+  delay(1);        // delay in between reads for stability
+}
